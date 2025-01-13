7e017e66d6e24958446f7da290d675873df7d24a
emily-titus <emmy.ctr2011@gmail.com>
2025-01-12 20:58:51 -0700

    Did the assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        System.Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        System.Console.WriteLine();
+        System.Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
