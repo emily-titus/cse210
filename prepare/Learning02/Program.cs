574392d767417aaaf90b8892a4433d06b5cfd5ed
emily-titus <emmy.ctr2011@gmail.com>
2025-01-22 20:59:20 -0700

    learning 02 complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Strawberry Farms";
+        job1._jobTitle = "Taste tester";
+        job1._startYear = 2024;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._company = "Yarn industries";
+        job2._jobTitle = "Professional Yarn Gremlin";
+        job2._startYear = 2022;
+        job2._endYear = 2080;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Emily Titus";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
     }
 }
